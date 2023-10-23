ef2e85ecf8e43087cbc77588678b5c980e7ab966
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-10-23 00:15:22 -0300

    :sparkles: finalizando aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LabelComponent } from './components/card/label/label.component';
+import { PriceComponent } from './components/card/price/price.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		CardComponent,
+		MenuComponent,
+		LabelComponent,
+		PriceComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/card/price/price.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-price',
+	templateUrl: './price.component.html',
+	styleUrls: ['./price.component.css'],
+})
+export class PriceComponent {
+	@Input() type: string = '';
+	@Input() price: string = '';
+}
